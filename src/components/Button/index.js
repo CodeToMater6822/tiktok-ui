0ef206658254dbc1d2d0a37fb7c83182adb09e2c
tung180999 <tungtungtung1235@gmail.com>
2024-07-07 21:13:03 +0700

    Cài đặt và cấu hình Prettier

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+
+function Button(){
+    const handleClick=()=>{
+        alert('Say hi!!!!!!!!')
+    }
+    return(
+        <button onClick={handleClick}>Click me!</button>
+    )
+}
+export default Button
